c21fd2949f509eb7d75cfcd46c24c86a3d4b904b
Shneypa Kalashnikov <osinfedor@gmail.com>
2014-12-15 21:12:26 +0300

    Fibonacci

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.math.BigInteger;
 
+import problems1to5.EvenFibonacciNumbers;
 import problems1to5.MultiplesOf3And5;
 
 
@@ -8,18 +9,21 @@ public class Main {
 	public static void main(String[] args) {
 		
 		// 1-5
-		// Multiples of 3 and 5 (or any two integers) 
+		// 1. Multiples of 3 and 5 (or any two integers) 
 		
 		int sum = MultiplesOf3And5.multiplesOfEitherOfTwoFactors(7,11,100);	
 		System.out.println();
 		
 		// Extended version: User inputs any amount of factors
-		MultiplesOf3And5.severalFactors();
+		// MultiplesOf3And5.severalFactors();
 		
 		// TODO 1. Apply a sorting algorithm to the resulting array 
 			
-		// Testing changes for GIT	
-		System.out.println("Testing GIT");
+		
+		// 2. Even Fibonacci Numbers
+		EvenFibonacciNumbers.fiboArray(4000000);
+		
+		// TODO 2. How to keep arrays properly sized? 
 	}
 
 }
